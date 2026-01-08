4ae2547da23312d6c4ae9cacc21e8db7be6b8c94
Eisia <98933225+EisiaDEV@users.noreply.github.com>
2026-01-08 17:05:40 +0900

    plugin internal name change

---
+++ src/main/java/com/eisiadev/enceladus/magicfind/MythicMagicFind.kt
@@ -0,0 +1,64 @@
+package com.eisiadev.enceladus.magicfind
+
+import com.eisiadev.enceladus.magicfind.listener.MythicMobDeathListener
+import com.eisiadev.enceladus.magicfind.pouch.PouchIntegration
+import com.eisiadev.enceladus.magicfind.util.MagicFindCalculator
+import com.eisiadev.enceladus.pouches.crayon.CrayonPouchManager
+import com.eisiadev.enceladus.pouches.powder.PowderPouchManager
+import com.eisiadev.enceladus.pouches.rift.RiftPouchManager
+import com.eisiadev.enceladus.pouches.soul.SoulPouchManager
+import com.eisiadev.enceladus.pouches.rune.RunePouchManager
+import org.bukkit.plugin.java.JavaPlugin
+
+class MythicMagicFind : JavaPlugin() {
+
+    val pouchIntegration = PouchIntegration(debug = false)
+
+    companion object {
+        private var _instance: MythicMagicFind? = null
+        val instance: MythicMagicFind
+            get() = _instance ?: throw IllegalStateException("Plugin not initialized")
+
+        fun isInitialized() = _instance != null
+    }
+
+    override fun onEnable() {
+        _instance = this
+
+        if (!server.pluginManager.isPluginEnabled("MythicMobs")) {
+            logger.severe("MythicMobs not found! Disabling plugin...")
+            server.pluginManager.disablePlugin(this)
+            return
+        }
+
+        if (!server.pluginManager.isPluginEnabled("Skript")) {
+            logger.severe("Skript not found! Disabling plugin...")
+            server.pluginManager.disablePlugin(this)
+            return
+        }
+
+        MagicFindCalculator.initialize(this)
+        server.pluginManager.registerEvents(MythicMobDeathListener(), this)
+
+        getCommand("magicfind")?.setExecutor(MagicFindCommand())
+
+        PowderPouchManager.initialize(this)
+        SoulPouchManager.initialize(this)
+        RunePouchManager.initialize(this)
+        CrayonPouchManager.initialize(this)
+        RiftPouchManager.initialize(this)
+
+        logger.info("MythicMagicFind has been enabled!")
+    }
+
+    override fun onDisable() {
+        PowderPouchManager.shutdown()
+        SoulPouchManager.shutdown()
+        RunePouchManager.shutdown()
+        CrayonPouchManager.shutdown()
+        RiftPouchManager.shutdown()
+
+        _instance = null
+        logger.info("MythicMagicFind has been disabled!")
+    }
+}
